c645fbb2f56eaf58de91cae2e93cadb835967afd
Chris Walter <walt50@gmail.com>
2020-05-13 13:14:06 -0400

    Initial Commit

---
+++ FriendFace/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FriendFace
+//
+//  Created by Christopher Walter on 5/13/20.
+//  Copyright © 2020 Christopher Walter. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
